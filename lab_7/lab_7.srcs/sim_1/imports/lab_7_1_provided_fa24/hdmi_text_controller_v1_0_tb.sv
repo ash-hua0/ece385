6862c6d3b5659cf6e8253a93a2ed20db166ec615
ash-hua0 <113153793+ash-hua0@users.noreply.github.com>
2024-10-29 12:12:09 -0500

    connected ports, clock wizard instantiated incorrectly
    hdmi_text_controller_v1_0.sv line 98, clock wizard instantiated improperly - pixel_clk is always 0 in sim

--- lab_7/lab_7.srcs/sim_1/imports/lab_7_1_provided_fa24/hdmi_text_controller_v1_0_tb.sv
+++ lab_7/lab_7.srcs/sim_1/imports/lab_7_1_provided_fa24/hdmi_text_controller_v1_0_tb.sv
@@ -111,19 +111,20 @@ module hdmi_text_controller_tb();
     //the pixel clock, assign pixel_clk = hdmi_text_controller_v1_0_inst.clk_25MHz
     
     // Red Green and Blue values respectively - these come from your draw logic
-    // assign pixel_rgb[0] = 
-    // assign pixel_rgb[1] =
-    // assign pixel_rgb[2] = 
+     assign pixel_rgb[0] = hdmi_text_controller_v1_0_inst.red;
+     assign pixel_rgb[1] = hdmi_text_controller_v1_0_inst.green;
+     assign pixel_rgb[2] = hdmi_text_controller_v1_0_inst.blue;
     
-    // Pixel clock, hs, vs, and vde (!blank) - these come from your internal VGA module
-    // assign pixel_clk = 
-    // assign pixel_hs = 
-    // assign pixel_vs = 
-    // assign pixel_vde = 
+    // Pixel clock, hs, vs, and vde (!blank)
+     assign pixel_clk = hdmi_text_controller_v1_0_inst.clk_25MHz;
+     assign pixel_hs = hdmi_text_controller_v1_0_inst.hsync;
+     assign pixel_vs = hdmi_text_controller_v1_0_inst.vsync;
+     assign pixel_vde =  hdmi_text_controller_v1_0.vde;
+
     
     // DrawX and DrawY - these come from your internal VGA module
-    // assign drawX = 
-    // assign drawY =
+     assign drawX = hdmi_text_controller_v1_0_inst.drawX;
+     assign drawY = hdmi_text_controller_v1_0_inst.drawY;
    
     // BMP writing task, based off work from @BrianHGinc:
     // https://github.com/BrianHGinc/SystemVerilog-TestBench-BPM-picture-generator
